22eb95d8f69e24eb17a1f298587c6ca809be057c
Babillabux <100165795+Babillabux@users.noreply.github.com>
2022-10-25 14:05:51 +0200

    Update Codes
    professor code

--- Miniville/CardsInfo.cs
+++ Miniville/CardsInfo.cs
@@ -9,34 +9,12 @@ namespace Miniville
 {
     internal class CardsInfo
     {
-        string _name;
-        string _color;
-        string _effect;
-
-        public string name { get { return _name; } set { _name = value; } }
-        public string color { get { return _color; } set { _color = value; } }
-        public string effect { get { return _effect; } set { _effect = value; } }
-
-        int _cost;
-        int _maxDice;
-        int _minDice;
-        int _gain;
-
-        public int cost { get { return _cost; } set { _cost = value; } }
-        public int maxDice { get { return _maxDice; } set { _maxDice = value; } }
-        public int minDice { get { return _minDice; } set { _minDice = value; } }
-        public int gain { get { return _gain; } set { _gain = value; } }
-
-        public CardsInfo(string name, string color, string effect, int cost, int maxSice, int minDice, int gain)
-        {
-            _name = name;
-            _color = color;
-            _effect = effect;
-
-            _cost = cost;
-            _maxDice = maxSice;
-            _minDice = minDice;
-            _gain = gain;
-        }
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Cost { get; set; }
+        public string Name { get; set; }
+        public string Effect { get; set; }
+        public int Dice { get; set; }
+        public int Gain { get; set; }
     }
 }
