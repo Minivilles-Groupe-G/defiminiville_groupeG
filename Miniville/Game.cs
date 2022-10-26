c00b0a5f4143c08bb317aa67bb8bd43512b4ab20
EwenR <ewenriallan@gmail.com>
2022-10-26 09:41:05 +0200

    Setting up variables and functions
    Setting up variables and functions in accordance with the ULM

--- Miniville/Game.cs
+++ Miniville/Game.cs
@@ -9,6 +9,60 @@ namespace Miniville
 {
     internal class Game
     {
-        
+        public bool turn;
+        public bool endGame;
+        public bool scoreGoal;
+        public int difficulty;
+
+        //Objet
+        private Player player = new Player();
+        private Player ai = new Player();
+
+        private List<int> canAIBuy;
+        //private Display display = new Display();
+        private Random random = new Random();
+        private Dice die;
+        public int dice = 0;
+        public int dice2 = 0;
+
+        public Game(bool turn, bool endGame, bool scoreGoal, int difficulty, Player player, Player ai, List<int> canAIBuy, Random random, Dice die, int dice, int dice2)
+        {
+            this.turn = turn;
+            this.endGame = endGame;
+            this.scoreGoal = scoreGoal;
+            this.difficulty = difficulty;
+            this.player = player;
+            this.ai = ai;
+            this.canAIBuy = canAIBuy;
+            this.random = random;
+            this.die = die;
+            this.dice = dice;
+            this.dice2 = dice2;
+        }
+
+        public void GameLoop()
+        {
+
+        }
+
+        public void PlayerTurn(Player player, Player opponent, string number)
+        {
+
+        }
+
+        public void AiTurn(Player ai, Player opponent, string number)
+        {
+
+        }
+
+        public bool CheckEndGame(int scoreGoal, int difficulty)
+        {
+
+        }
+
+        public void CheckPlayerWin(int scoreGoal)
+        {
+
+        }
     }
 }
